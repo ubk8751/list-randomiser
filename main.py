9947bee1e8b388aaee39dfd8c8846a241ad5b4cd
ubk8751 <ubk8751@gmail.com>
2023-01-25 14:16:35 +0100

    I think it's done?

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from random import shuffle
-l = []
-def main():
-    make_list("list.txt")
+from math import ceil
+
+def main(l:list = []):
+    l1, l2, l3, l4, l5 = ([] for i in range(5))
+    l = make_list("OG_list.txt", [])
     for _ in range(10):
-        do_the_shuffle()
-    l1, l2 = split()
-    print("l len: " + str(len(l)))
-    d = len(l1) + len(l2)
+        do_the_shuffle(l)
+    
+    s = split(5, [l1, l2, l3, l4, l5], l)
+    print("list len: " + str(len(l)))
+    d = 0
+    for lst in s:
+        d += len(lst)
     print("split len: " + str(d))
-    for l in [l1, l2]:
-        for itm in l:
-            print(itm)
-        print("\n")
+    print(write_file("split_list.txt", "divided_list.txt", s))
+    
 
 
-def make_list(file_name:str = "list.txt"):
+def make_list(file_name:str = "list.txt", l:list = []):
 # Using readlines()
     file1 = open(file_name, 'r')
     Lines = file1.readlines()
-    
     count = 0
     # Strips the newline character
     for line in Lines:
@@ -29,14 +31,41 @@ def make_list(file_name:str = "list.txt"):
         count += 1
         if line not in (l):
             l.append(line)
+    return l
 
-def do_the_shuffle():
+def do_the_shuffle(l:list = []):
     shuffle(l)
 
-def split():
-    l1 = l[0:len(l)//2]
-    l2 = l[len(l)//2:len(l)]
-    return l1,l2
+def split(n:int = 2, lists:list=[], l:list = []):
+    if lists == []:
+        return lists
+    step_size = len(l)//n
+    s = 0
+    e = step_size
+    for i in range(len(lists)):
+        s = i*step_size
+        e = i*step_size + step_size
+        lists[i] = l[s:e]
+    if e < len(l):
+        for i in range(len(lists)):
+            if e >= len(l):
+                break
+            lists[i].append(l[e])
+            e+=1
+    return lists
+
+def write_file(split_file:str = "split_list.txt", divided_file:str = "divided_list.txt", lsts:list = []):
+    if lsts == []:
+        return "Bruh, I need something to export!"
+    f = open(split_file, "w")
+    for l in lsts:
+        for itm in l:
+            f.write(itm + "\n")
+        EOL = "List length: " + str(len(l)) + "\n\n"
+        f.write(EOL)
+    return "File was created properly!? :D"
+    
 
 if __name__ == "__main__":
-    main()
+    l = []
+    main(l)
